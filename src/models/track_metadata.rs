71480227b035d7677f08987c89fe26bca5af77e0
cXp1r <2510037056@qq.com>
2026-05-15 21:03:13 +0800

    feat: 返回的歌词带上打分结果

--- src/models/track_metadata.rs
+++ src/models/track_metadata.rs
@@ -28,6 +28,7 @@ pub struct TrackMetadata {
     pub isrc: Option<String>,
     pub language: Option<Vec<String>>,
     pub note: Option<String>,
+    pub score: i8,
     pub trial: Option<[u32; 2]>,
     pub is_trial: bool,
 }
